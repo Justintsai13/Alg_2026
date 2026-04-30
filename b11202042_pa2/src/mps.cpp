31bc47e26acc245a20ecdea57786cdf0a39ecb3e
Justintsai13 <justintsai13@gmail.com>
2026-05-01 01:00:15 +0800

    mps function implementation no traceback

--- b11202042_pa2/src/mps.cpp
+++ b11202042_pa2/src/mps.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include <utility>
 using namespace std;
 
 
 // functions
-int MPS(int n, const int* C, int* M);
-
+int MPS(int n, int* C, int** m);
 vector<pair<int, int>> Traceback(int n, const int* C, int* M);
-
 void FindChord(int i, int j, int n, const int* C, int* M, vector<pair<int, int>>& r);
 
 int main(){
@@ -24,8 +23,46 @@ int main(){
         C[v] = u;
     }
 
-    cout << n << endl;
+    int** m = new int*[n];
     for (int i = 0; i < n; i++){
-        cout << C[i] << " ";
+        m[i] = new int[n - i]; // new table but upper triangle M(i, j) = M[i, j - i]
     }
+    for (int i = 0; i < n; i++){
+        m[i][0] = 0; // init to 0
+    }
+
+    int mps = MPS(n, C, m);
+
+    cout << mps << endl;
+
+    // cout << n << endl;
+    // for (int i = 0; i < n; i++){
+    //     cout << C[i] << " ";
+    // }
+    return 0;
+}
+
+
+// Function Implementation
+
+int MPS(int n, int* C, int** m){
+    for (int l = 1; l < n; l++){
+        for (int i = 0; i < n - l; i++){
+            int j = i + l;
+            int k = C[j];
+
+            if (k == i){ // case 3
+                m[i][l] = 1 + ((l == 1) ? 0 : m[i + 1][l - 2]);
+            }
+            else if (k < i || k > j){ // case 1
+                m[i][l] = m[i][l - 1];
+            }
+            else{ // case 2
+                int right = ((j == k + 1) ? 0 : m[k + 1][j - k - 2]);
+                m[i][l] = max(m[i][k - i - 1] + 1 + right, m[i][l - 1]);
+            }
+        }
+    }
+
+    return m[0][n - 1];
 }
